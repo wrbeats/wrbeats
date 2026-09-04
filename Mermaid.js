96e6b3ddd30856b05d5f5872cb3a5fadd309ad80
WR Beats <chicoloco.distru@gmail.com>
2026-09-04 19:04:22 +0000

    Add Mermaid graph definition to Mermaid.js

---
+++ Mermaid.js
@@ -0,0 +1,9 @@
+graph TD
+    A[CL40 World] --> C[الهواء]
+    B[WR Beats] --> D[Owner]
+    C --> E[Eye 👁️ For]
+    D --> F[An Eye]
+    E --> G[Conciencia]
+    F --> G[Conciencia]
+    
+    style G fill:#f9f,stroke:#333,stroke-width:2px
